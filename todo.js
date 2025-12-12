24c7be180133057f1fb83e578d3b97e091507fd0
Himanshu Warokar <warokarhimanshu12@gmail.com>
2025-12-12 14:45:54 +0530

    Implement basic todo list functionality

---
+++ todo.js
@@ -0,0 +1,37 @@
+const input = document.querySelector("#input");
+const add = document.querySelector("#add");
+const list = document.querySelector(".list");
+
+add.addEventListener("click",(e)=>{
+
+    if(!input.value) return;
+
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    const div = document.createElement('div');
+
+    div.classList.add("li-div");
+    btn.classList.add('del-but');
+
+    btn.innerHTML = "delete";
+    btn.style.marginTop= "10px";
+
+    li.innerHTML = input.value;
+
+    div.appendChild(li);
+    div.appendChild(btn);
+    list.appendChild(div);
+
+    btn.addEventListener('click',()=>{
+        li.remove();
+        btn.remove();
+    })
+    
+    input.value = "";
+    })
+
+
+
+
+    
+})
